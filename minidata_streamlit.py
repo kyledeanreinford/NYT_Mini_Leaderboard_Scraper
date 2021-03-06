8be96452bd2fc99fdcfac9cea7f4cd23e0cf7cdd
Kyle Dean Reinford <kyledeanreinford@gmail.com>
2021-03-06 06:37:25 -0600

    loaded sql data to streamlit

--- minidata_streamlit.py
+++ minidata_streamlit.py
@@ -8,14 +8,25 @@ from streamlit.report_thread import get_report_ctx
 engine = 'postgresql://miniadmin:acab@localhost:5432/minitimesdb'
 db = create_engine(engine)
 
+def load_data():
+    data = pd.read_sql("SELECT * FROM solves", con=db)
+    print(data)
+    st.write(data)
+
 if __name__ == '__main__':
     
     st.header('NYT Mini Leaderboard')
+    load_data()
+
 
     # players = []
-    # result = db.execute(text("SELECT * FROM solves WHERE name = 'Ericki'"))
+    # result = db.execute(text("SELECT name FROM solves"))
     # for row in result:
-    #     print(f"x: {row.name} y: {row.time}")
+    #     player = row.name
+    #     if player not in players:
+    #         players.append(player)
+    # for player in players:
+    #     print(player)
 
-    df = pd.read_sql_query("SELECT * FROM solves", con=db)
-    df
+    # df = pd.read_sql_query("SELECT * FROM solves", con=db)
+    # df
